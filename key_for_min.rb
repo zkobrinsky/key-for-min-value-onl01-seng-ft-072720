dca15ef987a1fb8317c5675282d92fa78a6b6d04
Zach Kobrinsky <zachkobrinsky@gmail.com>
2020-07-31 17:57:06 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,6 +6,10 @@ def key_for_min_value(name_hash)
 
   name_hash.each do |key, value}
 
-      if value < last_value
+    if value < last_value
+      last_value = value
+    else
+      return key
+    end
 
 end
