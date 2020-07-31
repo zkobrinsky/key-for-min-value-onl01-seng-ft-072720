23c8dcfbf69cb5256ac8f0658390339b829bf27c
Zach Kobrinsky <zachkobrinsky@gmail.com>
2020-07-31 17:51:13 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,10 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  last_value = 10000
+  
+  name_each.each do |key, value}
+    
+      if value < last_value
+    
 end
